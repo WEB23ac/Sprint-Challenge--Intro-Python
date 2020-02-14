9898b7ae874d5b0ff917454b2303c2ccf33b19a9
Aaron Clark <aaronpclark92@gmail.com>
2020-02-14 11:55:28 -0600

    complete 8/8 in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -68,8 +68,8 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 # print("All names uppercase:")
-# g = [entry.name.upper(), entry.age+5 for entry in humans]
-# print(g)
+g = [Human(entry.name.upper(), entry.age+5) for entry in humans]
+print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
